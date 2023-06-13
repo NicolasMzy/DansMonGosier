2d2f5f4ad5923b370d4dd93e6729cb67e0442c06
Barnabé Parisien <barnabeparisien@MacBook-Pro-de-Barnabe.local>
2023-06-13 12:40:30 +0200

    feat:client authenticate front

---
+++ APPs/Client/src/views/AuthenticateView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import FormAuth from '../components/FormAuth.vue'
+</script>
+
+<template>
+    <div class="formAuth">
+      <FormAuth />
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .formAuth {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
