e3873c3d9fb567f5290d582600b64ecb6b026e23
Mathieu CORMIER <mathieu.cormier@viacesi.fr>
2023-06-14 09:28:56 +0200

    feat : change app.js file to include itemRoutes

--- APIs/MicroservicesAPI/src/app.js
+++ APIs/MicroservicesAPI/src/app.js
@@ -1,1 +1,48 @@
-console.log('hello la bite');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+const itemRoutes = require('./services/items/items');
+
+app.use(express.json()) ;
+
+app.use(itemRoutes);
+
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  app.use((req, res, next) => {
+    console.log('Réception d\'une requête');
+    next();
+  });
+  
+  app.use((req, res, next) => {
+    res.status(201);
+    next();
+  });
+  
+  app.use((req, res, next) => {
+    res.json({ message: 'Votre requête a bien été reçue !!!!' });
+    next();
+  });
+  
+  app.use((req, res, next) => {
+    console.log('Réponse envoyée avec succès !');
+  });
+  
+module.exports = app;
+
+mongoose.connection.on('connected', function() {
+  console.log("database is ready now");
+});
+mongoose.connection.on('disconnected', function() {
+console.log("database is disconnected");
+});
+
+
