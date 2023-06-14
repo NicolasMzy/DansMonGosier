8032e849cf53470aed5550f78a29f27c70c59bb0
Mathieu CORMIER <mathieu.cormier@viacesi.fr>
2023-06-14 11:55:44 +0200

    feat : routes Create Seacrh, update, delete items

--- APIs/MicroservicesAPI/src/controllers/itemsControllers.js
+++ APIs/MicroservicesAPI/src/controllers/itemsControllers.js
@@ -1,19 +1,57 @@
-const createNewItem = require('../services/items/items');
+const itemsService = require('../services/items/itemsServices');
+// const itemsModel = require('../../models/mongodb/itemsSchema');
 
-exports.searchById = (req, res) => {
-    const id = req.params.id
-    res.send(`Id found : ${id}`)
-}
+exports.getItems = async (req, res) => {
+    try {
+      const { type, price, name, id } = req.query;
+  
+      const items = await itemsService.getItemsService(type, price, name, id);
+
+      res.status(200).json(items);
+    } catch (error) {
+      console.error('Erreur lors de la récupération des items :', error);
+      res.status(500).json({ message: 'Erreur lors de la récupération des items' });
+    }
+  };
+
 
 exports.createItem = async (req, res) => {
   try {
     const { name, description, price, type } = req.body;
 
-    const savedItem = await createNewItem(name, description, price, type);
+    const savedItem = await itemsService.createNewItem(name, description, price, type);
 
     res.status(201).json(savedItem);
   } catch (error) {
     console.error('Erreur lors de la création d\'un nouvel item :', error);
     res.status(500).json({ message: 'Erreur lors de la création d\'un nouvel item' });
   }
+};
+
+exports.updateItem = async (req, res) => {
+    try {
+      const itemId = req.params.itemId;
+      const { name, description, price, type } = req.body;
+  
+      const updatedItem = await itemsService.updateItem(itemId, name, description, price, type);
+  
+      res.json(updatedItem);
+    } catch (error) {
+      console.error('Erreur lors de la mise à jour de l\'item :', error);
+      res.status(500).json({ message: 'Erreur lors de la mise à jour de l\'item' });
+    }
+  };
+  
+
+exports.deleteItem = async (req, res) => {
+    try {
+        const itemId = req.params.itemId;
+
+        await itemsService.deleteItem(itemId);
+
+        res.json({ message: 'Item supprimé avec succès' });
+    } catch (error) {
+        console.error('Erreur lors de la suppression de l\'item :', error);
+        res.status(500).json({ message: 'Erreur lors de la suppression de l\'item' });
+    }
 };
--- APIs/MicroservicesAPI/src/routes/itemsRoutes.js
+++ APIs/MicroservicesAPI/src/routes/itemsRoutes.js
@@ -4,7 +4,15 @@ const router = express.Router();
 const itemsController = require('../controllers/itemsControllers'); 
 
 
+router.get('/items', itemsController.getItems);
+
 router.post('/items', itemsController.createItem);
+
+// PUT /items/:itemId
+router.put('/items/:itemId', itemsController.updateItem);
+
+// DELETE /items/:itemId
+router.delete('/items/:itemId', itemsController.deleteItem);
 
 
 module.exports = router;
