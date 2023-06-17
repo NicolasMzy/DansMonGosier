e04c572e3cfa84b5c49a6dfb5bf8f4e5fa645464
Barnabé Parisien <barnabeparisien@mbp-de-barnabe.home>
2023-06-17 15:15:07 +0200

    feat: register

--- APIs/MicroservicesAPI/account/server.js
+++ APIs/MicroservicesAPI/account/server.js
@@ -5,6 +5,12 @@ const PORT = process.env.PORT || 3000;
 const accountRoutes = require('./route');
 
 app.use(express.json());
+
+const cors = require('cors');
+app.use(cors({
+    origin: '*'
+}));
+
 app.use('/api', accountRoutes);
 
 app.listen(PORT, () => {
--- APPs/FrontEnd/src/components/FormRegister.vue
+++ APPs/FrontEnd/src/components/FormRegister.vue
@@ -1,22 +1,62 @@
 <script lang='ts' setup>
 import { ref } from 'vue'
+//import VuePhoneNumberInput from 'vue-phone-number-input';
+import axios from 'axios'
+//import 'vue-phone-number-input/dist/vue-phone-number-input.css';
+var email = document.getElementById('email')
+var pwd = ''
+var phone = ''
+var type = ''
 
+
+async function register() {
+    console.log(email)
+    try{
+        const {data} = await axios.post('http://localhost:3000/api/accounts', {
+        'email': email,
+        'pwd': pwd,
+        'phone': phone,
+        'type': type
+        }, {
+            headers: {
+            'Access-Control-Allow-Origin': true
+            }
+        }
+        )
+
+        alert('You are well registered')
+    } catch(error) {
+        alert(error)
+    }
+    
+}
 </script>
 
 <template>
-        <form action="#">
+        <form v-on:submit="register()">
             <label for="name">Name:</label><br>
             <input type="text" id="name" name="name" placeholder="Name"><br>
             <label for="surname">Surname:</label><br>
             <input type="text" id="surname" name="surname" placeholder="Surname"><br>
             <label for="email">Email:</label><br>
-            <input type="text" id="email" name="email" placeholder="Email"><br>
+            <input type="text" id="email" name="email" placeholder="Email" v-model="email"><br>
+
+            <label for="phone">Phone:</label><br>
+            <input type="number" id="phone" name="phone" placeholder="Phone" v-model="phone"><br>
+
+
             <label for="pass">Password:</label><br>
-            <input type="text" id="pass" name="pass" placeholder="Password"><br>
+            <input type="text" id="pass" name="pass" placeholder="Password" v-model="pwd"><br>
+            <label for="select-type">What will you be?</label><br>
+            <select v-model="type" id="select-type">
+                <option value="User">User</option>
+                <option value="Restaurant">Restaurant</option>
+                <option value="Delivery Person">Delivery Person</option>
+            </select>
+
             <a href="#" class="forget">Forgot password?</a>
-            <router-link to="/"><input type="submit" value="Submit"></router-link>
+            <input type="submit" value="Submit">
             
         </form> 
 <br>
 </template>
-  
