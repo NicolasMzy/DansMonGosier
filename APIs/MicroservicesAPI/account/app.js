1142af941e83ec03cf2f232e40d09c86ba57c1a3
Skane-Gahi <skanguy17@orange.fr>
2023-06-20 16:41:20 +0200

    refactor: jwt token now contains user type

--- APIs/MicroservicesAPI/account/app.js
+++ APIs/MicroservicesAPI/account/app.js
@@ -14,7 +14,7 @@ const main = async () => {
 
   app.use(express.json());
 
-  app.use('/api', accountRoutes);
+  app.use('/', accountRoutes);
 
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
--- APIs/MicroservicesAPI/account/microservice.js
+++ APIs/MicroservicesAPI/account/microservice.js
@@ -1,7 +1,7 @@
 const sequelize = require('./config/db');
 const models = require('./models/index');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 
 
 exports.create = async (email, phoneNb, password, userType) => {
@@ -18,7 +18,7 @@ exports.create = async (email, phoneNb, password, userType) => {
 };
 
 exports.login = async (email, password) => {
-    const account = await models.Credentials.findAll({
+    var account = await models.Credentials.findAll({
         where: {
             email: email
         }
@@ -26,12 +26,17 @@ exports.login = async (email, password) => {
     if (account.length == 0 || !bcrypt.compareSync(password, account[0].dataValues.pwd)) {
         return false;
     }
+    
+    account = account[0].dataValues;
+    const data = {
+        id: account.id,
+        email: account.email,
+        userType: account.user_type
+    };
+    const token = jwt.sign(data, 'your_secret_key', { expiresIn: '1h' });
 
-    const token = jwt.sign({ id: account.id }, 'your_secret_key', { expiresIn: '1h' });
-
-    return token //json({ message: 'Logged in successfully', token });
+    return token;
 };
-
 
 exports.getAll = async () => {
     await sequelize.sync();
