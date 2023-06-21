cf77d2719892db4a3aa69e0e201b837912b04bd0
Barnabé <barnab.parisien@gmail.com>
2023-06-21 22:00:11 +0200

    fix: refactor front client

--- APPs/Client/src/router/index.ts
+++ APPs/Client/src/router/index.ts
@@ -9,17 +9,9 @@ const router = createRouter({
       component: () => import('@/views/HomeView.vue')
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('@/views/AboutView.vue')
-    },
-    {
-      path : '/auth',
-      name: 'authenticate',
-      component: () => import('@/views/AuthenticateView.vue')
+      path: '/register',
+      name: 'register',
+      component: () => import('@/views/RegisterView.vue')
     }
   ]
 })
---
+++ APPs/Client/src/views/PaymentView.vue
@@ -0,0 +1,14 @@
+<template>
+  
+</template>
+
+<script lang="ts">
+export default {
+    name: 'PaymentView'
+
+}
+</script>
+
+<style>
+
+</style>
