dd12b1f0b5dade41817655214f145024bf26041c
Barnabé <barnab.parisien@gmail.com>
2023-06-27 23:53:42 +0200

    feat: started restaurantfront

--- APPs/Restaurant/src/views/RestaurantHome.vue
+++ APPs/Restaurant/src/views/RestaurantHome.vue
@@ -1,39 +1,39 @@
 <template>
   <div class="home">
-    <Header/>
-    {{ order }}
+    <h1>Order List</h1>
+    <ul>
+      <li v-for="order in orders" :key="order.id_restaurant">
+        {{ order.label }} - {{ order.price }}
+      </li>
+    </ul>
   </div>
 </template>
 
-<script  lang="ts">
-import { onMounted, ref, defineComponent } from 'vue'
-//IMPORT
-import Header from '../components/componentsGlobaux/Header.vue';
-import axios from 'axios'
+<script lang="ts">
+import axios from 'axios';
+import type {Order} from '../types/Order'
+import { onMounted } from 'vue'
 
-export default defineComponent({
-  name : 'RestaurantHome',
-  components: {
-    Header,
-  },
-  setup() {
-    let order = ref(null);
-    onMounted(async () => {
-        const orders = await axios.get('http://localhost:3012/orders');
-        order.value = orders.data;
+export default {
+  setup(){
 
-      
-    });
-    return{
-      order,
+  let orders: Order[] = [];
 
-    }
+  onMounted(async () => {
+      try {
+        const response = await axios.get('/api/orders');
+        orders = response.data;
+      } catch (error) {
+        console.error('Error fetching orders:', error);
+      }
+  });
+
+  return{
+    orders,
   }
-});
-  
+}
+ 
+}
+
 
 </script>
-
-<style>
-
-</style>
