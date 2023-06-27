e810b3be066281ae99b864ca642653f709da1947
NicolasMzy <nicolasmazy1@gmail.com>
2023-06-27 09:53:40 +0200

    refactor : redisign settings Restaurant APP

--- APPs/Restaurant/src/router/index.ts
+++ APPs/Restaurant/src/router/index.ts
@@ -1,23 +1,57 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      redirect: '/home',
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: '/home',
+      name: 'ClientHome',
+      component: () => import('../views/restaurant/RestaurantHome.vue'),
+    },
+    {
+      path: '/settings',
+      name: 'SettingsVue',
 
-export default router
+      children: [
+        {
+          path: '',
+          name: 'Settings',
+          component: () => import('../views/settings/SettingsVue.vue'),
+        },
+        {
+          path: 'profileinformation',
+          name: 'ProfileInformation',
+          component: () => import('../views/settings/ProfileInformation.vue'),
+        },
+        {
+          path: 'changepassword',
+          name: 'ChangePassword',
+          component: () => import('../views/settings/ChangePassword.vue'),
+        },
+        {
+          path: 'paymentmethods',
+          name: 'PaymentMethods',
+          component: () => import('../views/settings/PaymentsMethods.vue'),
+        },
+        {
+          path: 'location',
+          name: 'Location',
+          component: () => import('../views/settings/Location.vue'),
+        },
+        {
+          path: 'refertofriends',
+          name: 'ReferTofriend',
+          component: () => import('../views/settings/ReferToFriend.vue'),
+        },
+      ],
+    },
+  ],
+});
+
+export default router;
