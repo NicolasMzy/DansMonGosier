54cbba1a02aedca8227e28ba26eb5ff3d5b1e26a
Skane-Gahi <skanguy17@orange.fr>
2023-06-28 10:41:43 +0200

    feat: account id added to login response

--- apis/auth/src/microservice.js
+++ apis/auth/src/microservice.js
@@ -37,5 +37,5 @@ exports.login = async (email, password) => {
   };
   const token = jwt.sign(data, process.env.JWT_SECRET_KEY, {expiresIn: '1h'});
 
-  return token;
+  return {id: account.id, token: token};
 };
