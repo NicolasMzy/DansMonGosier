57af7906ae790bdfaef002c6b1f119612a300a75
Barnabé <barnab.parisien@gmail.com>
2023-06-28 09:44:35 +0200

    fix: ca me clc

--- APPs/Client/src/types/Rate.ts
+++
@@ -1,6 +0,0 @@
-export interface Rate {
-    id_user: number,
-    rate: number,
-    comment: string,
-    date: Date
-}
--- APPs/Restaurant/src/types/ItemType.ts
+++
@@ -1,6 +0,0 @@
-export enum ItemType {
-    Plat = 'plat',
-    Boisson = 'boisson',
-    Sauce= 'sauce',
-    Accompagnement = 'accompagnement',
-}
