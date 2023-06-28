06db7dd84e88012c99a849fbf80aaad7a88cce6a
Barnabé <barnab.parisien@gmail.com>
2023-06-28 09:39:40 +0200

    fix: jsp trop 988 changements hmmm

--- APPs/FrontEnd/src/types/Restaurant.ts
+++
@@ -1,7 +0,0 @@
-export interface Restaurant {
-        image: string;
-        restaurantName: string;
-        note: string;
-        drive_time: string;
-        //price: string;
-    }
--- APPs/Restaurant/src/types/Menu.ts
+++
@@ -1,9 +0,0 @@
-import type {Item} from "./Item"
-
-export interface Menu {
-    label: string,
-    description: string,
-    photo: string,
-    price: number,
-    items: Item[],
-}
