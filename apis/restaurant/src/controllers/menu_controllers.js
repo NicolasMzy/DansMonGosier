f78a1cd0098bd11a30b017ce189de03d6b7008e1
Mathieu CORMIER <mathieu.cormier@viacesi.fr>
2023-06-29 14:22:14 +0200

    feat : routes for stats changed

--- apis/restaurant/src/controllers/menu_controllers.js
+++ apis/restaurant/src/controllers/menu_controllers.js
@@ -151,3 +151,45 @@ exports.addItemToMenu = async (req, res) => {
     return res.status(500).json({ message: 'Server error', error });
   }
 };
+
+exports.updateMenuItem = async (req, res) => {
+  const { restaurantId, menuId, itemId } = req.params;
+  const { label, description, photo, price, type, quantity } = req.body;
+
+  if (!label || !description || !photo || !price || !type || !quantity) {
+    return res.status(400).json({ message: 'Missing required fields: label, description, photo, price, type, quantity' });
+  }
+
+  try {
+    const restaurant = await Restaurant.findOne({ id_credentials: restaurantId });
+
+    if (!restaurant) {
+      return res.status(404).json({ message: 'Restaurant not found' });
+    }
+
+    const menu = restaurant.menus.id(menuId);
+
+    if (!menu) {
+      return res.status(404).json({ message: 'Menu not found' });
+    }
+
+    const menuItem = menu.items.id(itemId);
+
+    if (!menuItem) {
+      return res.status(404).json({ message: 'Item not found' });
+    }
+
+    menuItem.item.label = label;
+    menuItem.item.description = description;
+    menuItem.item.photo = photo;
+    menuItem.item.price = price;
+    menuItem.item.type = type;
+    menuItem.quantity = quantity;
+
+    await restaurant.save();
+
+    return res.status(200).json({ message: 'Item updated successfully', restaurant });
+  } catch (error) {
+    return res.status(500).json({ message: 'Server error', error });
+  }
+};
